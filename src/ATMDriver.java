43acdfce16dabf4535ed4233deb17ac68f0de7ca
Nicholas Erickson <nicholae@tmctrans.com>
2023-04-07 14:12:50 -0500

    update

--- src/ATMDriver.java
+++ src/ATMDriver.java
@@ -5,6 +5,7 @@ public class ATMDriver {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         int menuSelect = 0;
+        int subMenuSelect = 0;
         User user = new User();
 
         System.out.println("Starting Application");
@@ -42,7 +43,63 @@ public class ATMDriver {
             }
 
             if (menuSelect == 1) {
-                
+                while(true){
+                    System.out.println("Please Select ATM Action Below:");
+                    System.out.println("1: Check Balance");
+                    System.out.println("2: Deposit");
+                    System.out.println("3: Withdraw");
+                    System.out.println("4: Transfer");
+                    System.out.println("5: Exit");
+
+                    while(true) {
+                        System.out.print("Input: ");
+                        subMenuSelect = scan.nextInt();
+                        System.out.println();
+                        if (menuSelect < 1 || menuSelect > 5) {
+                            System.out.println("Invalid Input");
+                        } else {
+                            break;
+                        }
+                    } 
+
+                    if (subMenuSelect == 1) {
+                        System.out.println("Which account? \n (1) Checking (2) Savings");
+                        System.out.print("Input: ");
+                        subMenuSelect = scan.nextInt();
+
+                        if (subMenuSelect == 1) {
+                            System.out.println("Balance: $" + checkAcc.getBalance());
+                        } else {
+                            System.out.println("Savings: $" + saveAcc.getBalance());
+                        }
+                    } else if (subMenuSelect == 2) {
+                        System.out.println("Which account? \n (1) Checking (2) Savings");
+                        System.out.print("Input: ");
+                        subMenuSelect = scan.nextInt();
+
+                        System.out.println("How much?");
+                        System.out.print("Input: ");
+                        int deposit = scan.nextInt();
+
+                        if (subMenuSelect == 1) {
+                            if ((checkAcc.getAmountDepo() + deposit) > 5000) {
+                                System.out.println("Can only deposit " + (5000 - checkAcc.getAmountDepo()) + " more today.");    
+                            } else {
+                                System.out.println(deposit + " successfully deposited.\n");
+                                checkAcc.setBalance(checkAcc.getBalance() + deposit);
+                                checkAcc.setAmountDepo(checkAcc.getAmountDepo() + deposit);
+                            }
+                        } else {
+                            System.out.println("Savings: $" + saveAcc.getBalance());
+                        }
+                    } else if (subMenuSelect == 3) {
+
+                    } else if (subMenuSelect == 4) {
+
+                    } else {
+                        break;
+                    }
+                }
             } else if (menuSelect == 2) {
 
             } else if (menuSelect == 3) {
@@ -53,6 +110,9 @@ public class ATMDriver {
         }
         System.out.println("Exiting Application");
 
+        DataAccess.setCheckingAccount(checkAcc);
+        DataAccess.setSavingsAccount(saveAcc);
+        // DataAccess.setDayNum(dayNum);
         scan.close();
     }
 
--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,6 +1,7 @@
 public class CheckingAccount {
-    public long balance;
-    public long amountWithdrawn;
+    private long balance;
+    private long amountWithdrawn;
+    private long amountDepo;
 
     public long getBalance() {
         return this.balance;
@@ -10,11 +11,19 @@ public class CheckingAccount {
         return this.amountWithdrawn;
     }
 
+    public long getAmountDepo() {
+        return this.amountDepo;
+    }
+
     public void setBalance(long bal) {
         this.balance = bal;
     }
     
     public void setAmountWithdrawn(long amount) {
         this.amountWithdrawn = amount;
+    }
+
+    public void setAmountDepo(long amount) {
+        this.amountDepo = amount;
     }
 }
